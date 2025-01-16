0fa2494a43d6d2f5705d4672145fe64bfd28e65c
Robson Barbosa <robsonbarbosa.dev@gmail.com>
2025-01-15 21:44:16 -0300

    refactor: mock

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Card from "./components/Card/Card";
 import ViewTask from "./components/ViewTask/ViewTask";
 import { taskTypes } from "./lib/types/board.types";
 import NewColumn from "./components/NewColumn/NewColumn";
+import { DndContext } from "@dnd-kit/core";
 
 export interface ViewTaskModal {
   open: boolean;
@@ -27,9 +28,9 @@ function App() {
 
   return (
     <div className="h-full pl-[0.5625rem]">
-      {mockBoard.columns.length > 0 ? (
+      {mockBoard[0].columns.length > 0 ? (
         <div className="flex h-full">
-          {mockBoard.columns.map((column) => (
+          {mockBoard[0].columns.map((column) => (
             <div key={column.id} className="w-72 rounded-md p-3">
               <div className="flex justify-between items-center mb-6 mt-3">
                 <div className="text-heading-sm font-bold text-body-text-secondary tracking-[0.1rem] flex">
@@ -40,10 +41,22 @@ function App() {
                   {column.name} ({column.tasks.length})
                 </div>
               </div>
+              {/* {column.tasks.length > 0 ? (
+                <>
+                  {column.tasks.map((task, id) => (
+                    <div>{task.id}</div>
+                  ))}
+                </>
+              ) : (
+                <div className="text-gray-500">
+                  This column is empty. Create a new task to get started.
+                </div>
+              )} */}
               {column.tasks.length > 0 ? (
                 <>
                   <div className="flex gap-5 flex-col">
                     {column.tasks.map((task, id) => (
+                      // <div>{task.id}</div>
                       <Card
                         title={task.name}
                         key={id}
@@ -70,13 +83,13 @@ function App() {
             </div>
           ))}
           <div className="flex justify-center items h-[50.875rem] w-[17.5rem] bg-[#E9EFFA] mr-10 rounded-md items-center mt-16 ml-3">
-          <Button
-            variant="outline"
-            className="text-body-text-secondary mb-2 font-bold text-xl hover:text-primary"
-            onClick={() => setModalNewColumn(true)}
-          >
-            + New Column
-          </Button>
+            <Button
+              variant="outline"
+              className="text-body-text-secondary mb-2 font-bold text-xl hover:text-primary"
+              onClick={() => setModalNewColumn(true)}
+            >
+              + New Column
+            </Button>
           </div>
         </div>
       ) : (
@@ -95,7 +108,7 @@ function App() {
       )}
       {modal && <ViewTask modal={modal} setModal={setModal} />}
       {modalNewColumn && (
-        <NewColumn modal={modalNewColumn} setModal={setModalNewColumn}/>
+        <NewColumn modal={modalNewColumn} setModal={setModalNewColumn} />
       )}
     </div>
   );
--- src/lib/mocks/board.mocks.ts
+++ src/lib/mocks/board.mocks.ts
@@ -1,46 +1,72 @@
-import { boardTypes, columnTypes, subTaskTypes, taskTypes } from '../types/board.types';
-
-const mockSubTasks: subTaskTypes[] = [
-  { id: 'sub1', name: 'Sub Task 1', isCompleted: false },
-  { id: 'sub2', name: 'Sub Task 2', isCompleted: true },
-];
-
-const mockTasks: taskTypes[] = [
-  { id: '1', name:  'Build UI for onboarding flow', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: false, subTasks: mockSubTasks, column: 'todo' },
-  { id: '2', name:  'Build UI for search', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'todo'},
-  { id: '3', name:  'Create template structures', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'todo'},
-  { id: '4', name:  'QA and test all major user journeys', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'todo'},
-];
-
-const mockTasksDoing: taskTypes[] = [
-  { id: '5', name: 'Design settings and search pages', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: false, subTasks: mockSubTasks, column: 'doing'},
-  { id: '6', name: 'Add account management endpoints', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'doing'},
-  { id: '7', name: 'Design onboarding flow', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'doing'},
-  { id: '8', name: 'Add search enpoints', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'doing'},
-  { id: '9', name: 'Add authentication endpoints', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'doing'},
-];
-
-const mockTasksDone: taskTypes[] = [
-  { id: '10', name: 'Conduct 5 wireframe tests', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: false, subTasks: mockSubTasks, column: 'done'},
-  { id: '11', name: 'Create wireframe prototype', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'done'},
-  { id: '12', name: 'Review results of usability tests and iterate', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'done'},
-  { id: '13', name: 'Create paper prototypes and conduct 10 usability tests with potential customers', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'done'},
-  { id: '14', name: 'Market discovery', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'done'},
-  { id: '15', name: 'Competitor analysis', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'done'},
-  { id: '16', name: 'Research the market', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra est non massa vehicula aliquam.', isCompleted: true, subTasks: mockSubTasks, column: 'done'},
-];
-
-
-const mockColumns: columnTypes[] = [
-  { id: '1', name: 'TODO', tasks: mockTasks, color: '#49C4E5' },
-  { id: '2', name: 'DOING', tasks: mockTasksDoing, color: '#8471F2' },
-  { id: '3', name: 'DONE', tasks: mockTasksDone, color: '#67E2AE' },
-];
-
-const mockBoard: boardTypes = {
-  id: 'board1',
-  name: 'Board 1',
-  columns: mockColumns,
+type Task = {
+  id: number;
+  name: string;
+  description: string;
+  isCompleted: boolean;
+  subTasks: SubTask[];
 };
+
+type SubTask = {
+  id: number;
+  name: string;
+  description: string;
+  isCompleted: boolean;
+};
+
+type Column = {
+  id: number;
+  name: string;
+  tasks: Task[];
+};
+
+type Board = {
+  id: number;
+  name: string;
+  color: string;
+  columns: Column[];
+}[];
+const mockBoard: Board = [{
+  id: 1,
+  name: "Board 1",
+  color: "#FF0000",
+  columns: [
+    {
+      id: 1,
+      name: "TODO",
+      tasks: [
+        {
+          id: 1,
+          name: "Task 1",
+          description: "Task 1 description",
+          isCompleted: false,
+          subTasks: [{
+            id: 1,
+            name: "SubTask 1",
+            description: "SubTask 1 description",
+            isCompleted: false,
+          }]
+        }
+      ],
+    },
+       {
+      id: 2,
+      name: "DOING",
+      tasks: [
+        {
+          id: 1,
+          name: "Task 2",
+          description: "Task 2 description",
+          isCompleted: false,
+          subTasks: [{
+            id: 1,
+            name: "SubTask 2",
+            description: "SubTask 2 description",
+            isCompleted: false,
+          }]
+        }
+      ],
+    }
+  ]
+}];
 
 export default mockBoard;
